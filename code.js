26257785644701c5ea9cdd15ab81a9be8d2b3e74
makihaya <93355988+makihaya@users.noreply.github.com>
2023-01-11 15:16:44 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,40 +1,50 @@
 function createNote(){
-    const deleteNote = document.createElement("span");
-      deleteNote.innerHTML = "🗑";
-      deleteNote.classList.add("delete-p");
-    
-    const noteDisplay = document.getElementById("note-display");
-    const note = document.createElement("textarea");
-      note.classList.add("note");
-    
-    const colorChoice = document.getElementById("select-color");
-    const color = colorChoice.options[colorChoice.selectedIndex].text;
-  
-    if(color == "White"){
-       note.classList.add("white");
-    } else if (color == "Pink"){
-       note.classList.add("pink");
-    } else if (color == "Yellow"){
-       note.classList.add("yellow");
-    } else if (color == "Green"){
-       note.classList.add("green");
-    } else if (color == "Blue"){
-       note.classList.add("blue");
-    } else if (color == "Purple"){
-       note.classList.add("purple");
-    }
+   //create delete button
+   const deleteNote = document.createElement("img");
+     deleteNote.src = "images/Trash_Icon.png";
+     deleteNote.classList.add("delete-p");
    
-    noteDisplay.appendChild(note);
-    noteDisplay.appendChild(deleteNote);
+   //create const for note display area
+   const noteDisplay = document.getElementById("note-display");
+    
+   //create note as a textarea
+   const note = document.createElement("textarea");
+     note.classList.add("note");
+    
+   //create const for color choice dropdown
+   const colorChoice = document.getElementById("select-color");
+    
+   //create const for selected color option
+   const color = colorChoice.options[colorChoice.selectedIndex].text;
+  
+   //add a different class depending on selected color choice
+   if(color == "White"){
+      note.classList.add("white");
+   } else if (color == "Pink"){
+      note.classList.add("pink");
+   } else if (color == "Yellow"){
+      note.classList.add("yellow");
+   } else if (color == "Green"){
+      note.classList.add("green");
+   } else if (color == "Blue"){
+      note.classList.add("blue");
+   } else if (color == "Purple"){
+      note.classList.add("purple");
+   }
    
-    deleteNote.addEventListener("click", ()=>{
-      noteDisplay.removeChild(note);
-      noteDisplay.removeChild(deleteNote);
-    });
-  };
+   //append note and delete button to parent
+   noteDisplay.appendChild(note);
+   noteDisplay.appendChild(deleteNote);
+   
+   //when delete button is clicked, remove note and delete button from parent
+   deleteNote.addEventListener("click", ()=>{
+     noteDisplay.removeChild(note);
+     noteDisplay.removeChild(deleteNote);
+   });
+};
   
-  
+  //when clear all button is clicked, make note display area blank
   clear.addEventListener("click", ()=>{
-    document.getElementById("note-display").innerHTML = "";
+     document.getElementById("note-display").innerHTML = "";
   });
   
